1210234bb228c4be6946d95e9535c59e1472b437
Falak0891 <70525535+Falak0891@users.noreply.github.com>
2023-02-28 16:52:41 +0530

    Update Details.jsx

--- src/pages/app/details/Details.jsx
+++ src/pages/app/details/Details.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import style from "./Details.module.css";
 import coursesData from "../../../data/courses.json";
+//Third task: import useParams hook
 
 function Details() {
   return (
+    //Third Task: get course id and match it with the data
+    
     <div className={style.courses_container}>
       <div className={style.card_container}>
         <div className={style.card_image}>
           <div className={style.image_container}>
-            <img src={course.img} alt="icons" />
+            {/*Third task: image source here */}
+            <img src="**IMAGE**" alt="icons" />
           </div>
         </div>
         <div className={style.card_content}>
-          <h1 className={style.card_title}>{course.title}</h1>
-          <p className={style.card_description}>{course.description}</p>
+          {/*Third Task:  Title and Description here */}
+          <h1 className={style.card_title}>**TITLE**</h1>
+          <p className={style.card_description}>**DESCRIPTION**</p>
         </div>
       </div>
 
