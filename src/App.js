27a3202f1e6b03d5c9217a7865e02e8c65841bf3
falak Chandni <falak.chandni@codingninjas.com>
2023-02-28 16:10:49 +0530

    createBowserRouter added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 function App() {
+  const browserRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav />,
+      children: [
+        { path: "", element: <Hero /> },
+        { path: "/courses", element: <Courses /> },
+      ],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <Courses />
-    </div>
+    <>
+      <RouterProvider router={browserRouter} />
+    </>
   );
 }
 
